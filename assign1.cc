11528e4e53c463b6e8f0f552646d8002ca1489f2
Nathaniel Camorlinga <z2010945@turing.cs.niu.edu>
2023-09-08 20:21:37 -0500

    wrote my name

---
+++ assign1.cc
@@ -0,0 +1,13 @@
+/*************************************************************************
+* NIU CSCI 340 0002
+* Assignment 1
+* Nathaniel Camorlinga z2010945 natec2010945
+* 
+* I certify that everything I am submitting is either provided by the professor for use in * * 
+*the assignment, or work done by me personally. I understand that if I am caught submitting 
+*the work of others (including StackOverflow or ChatGPT) as my own is an act of Academic
+* Misconduct and will be punished as such.
+
+
+/***********************************************************************
+
